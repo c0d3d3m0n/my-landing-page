890051eff63e0d8416570ba4e30e0cb45d0f0c34
Jamey B. <jamey@jybryce.dev>
2024-07-15 14:10:23 -0700

    import React needed for jest

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export default function MainContainer({ children }) {
   return (
-    <div className='flex flex-col justify-center items-center min-h-screen'>
+    <div className='flex flex-col justify-center items-center min-h-screen main-container'>
       <Header />
       <main className='flex-grow mt-10'>{children}</main>
       <Footer />
